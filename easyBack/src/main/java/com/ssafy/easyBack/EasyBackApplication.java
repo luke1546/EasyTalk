f752df61e3569c798ec2196d8710c29e40067a1c
hoseong <luke1546@naver.com>
2024-01-24 14:38:07 +0900

    init: 백엔드생성

---
+++ easyBack/src/main/java/com/ssafy/easyBack/EasyBackApplication.java
@@ -0,0 +1,13 @@
+package com.ssafy.easyBack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasyBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EasyBackApplication.class, args);
+	}
+
+}
---
+++ easyBack/src/test/java/com/ssafy/easyBack/EasyBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssafy.easyBack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EasyBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
