0ccaab121f7f831ce213f989a0e67822c1b61dba
신성현 <froggy0m0a@gmail.com>
2024-02-13 01:29:57 +0900

    Test: 테스트를 위해 기존프로젝트 삭제

--- easyFront/src/components/pages/Group/GroupOpenPage.js
+++
@@ -1,8 +0,0 @@
-import Input from "../../../components/UI/atoms/Input/Input";
-import Textbox from "../../../components/UI/atoms/Text/Textbox";
-
-const GroupOpenPage = () => {
-  return <div className="GroupOpenPage"></div>;
-};
-
-export default GroupOpenPage;
