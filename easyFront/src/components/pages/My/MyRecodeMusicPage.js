3fda7736332348bfb045ac2b0f021f6f468e16d7
전재우 <crossly_t@naver.com>
2024-02-13 11:13:33 +0900

    test_error_edit

--- easyFront/src/components/pages/My/MyRecodeMusicPage.js
+++ easyFront/src/components/pages/My/MyRecodeMusicPage.js
@@ -1,61 +1,61 @@
-import React, { useState, useEffect } from 'react';
-import axios from "axios";
-import TabBar from '../../UI/modules/TabBar/TabBar';
-import Button from "../../UI/atoms/Button/Button";
+// import React, { useState, useEffect } from 'react';
+// import axios from "axios";
+// import TabBar from '../../UI/modules/TabBar/TabBar';
+// import Button from "../../UI/atoms/Button/Button";
 
-const MyRecodeMusicPage = () => {
-  const [myrecodemusic, setmyrecodemusic] = useState([]);
+// const MyRecodeMusicPage = () => {
+//   const [myrecodemusic, setmyrecodemusic] = useState([]);
 
-  const fetchMyRecodeMusic = async () => {
-    try {
-      const params = {
-        target: "music",
-      };
+//   const fetchMyRecodeMusic = async () => {
+//     try {
+//       const params = {
+//         target: "music",
+//       };
 
-      const response = await axios.get("/study/test/record", { params }, { withCredentials: true });
+//       const response = await axios.get("/study/test/record", { params }, { withCredentials: true });
 
-      setmyrecodemusic(response.data)
-    } catch (error) {
-      console.error("Error myrecodemusic", error);
-    }
-  };
+//       setmyrecodemusic(response.data)
+//     } catch (error) {
+//       console.error("Error myrecodemusic", error);
+//     }
+//   };
 
-  useEffect(() => {
-    fetchMyRecodeMusic();
-  }, []);
+//   useEffect(() => {
+//     fetchMyRecodeMusic();
+//   }, []);
 
-  const RecodeTabs = [
-    { label: '노래시험', to: '/myrecodemusic' },
-    { label: '단어시험', to: '/myrecodeword' },
-  ]
+//   const RecodeTabs = [
+//     { label: '노래시험', to: '/myrecodemusic' },
+//     { label: '단어시험', to: '/myrecodeword' },
+//   ]
 
-    // 저장 모드 상태
-    const [issaving, setIssaving] = useState(false);
-    // 저장 버튼 클릭 -> 저장 모드 상태
-   const handleSaveClick = () => {
-      setIssaving(!issaving);
-   };
+//     // 저장 모드 상태
+//     const [issaving, setIssaving] = useState(false);
+//     // 저장 버튼 클릭 -> 저장 모드 상태
+//    const handleSaveClick = () => {
+//       setIssaving(!issaving);
+//    };
  
 
-  // return (
-  //   <>
-  //     <TabBar tabs={RecodeTabs} />
-  //     <div onClick={() => handleSaveClick()}>
-  //       <Button
-  //         name="submitBtn"
-  //         text={issaving? '저장완료' : '저장'}
-  //         color={issaving ? 'red' : 'blue'}
-  //         />
-  //     </div>
-  //     <div>
-  //       {myrecodemusic.map((remusic, index) => (
-  //         <div key={index}>
+//   // return (
+//   //   <>
+//   //     <TabBar tabs={RecodeTabs} />
+//   //     <div onClick={() => handleSaveClick()}>
+//   //       <Button
+//   //         name="submitBtn"
+//   //         text={issaving? '저장완료' : '저장'}
+//   //         color={issaving ? 'red' : 'blue'}
+//   //         />
+//   //     </div>
+//   //     <div>
+//   //       {myrecodemusic.map((remusic, index) => (
+//   //         <div key={index}>
             
-  //       ))}
-  //     </div>
+//   //       ))}
+//   //     </div>
 
-  //   </>
-  // )
-};
+//   //   </>
+//   // )
+// };
 
-export default MyRecodeMusicPage;
+// export default MyRecodeMusicPage;
