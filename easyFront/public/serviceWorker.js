a1f374233e2da5f8f63a6059982ab08f832cc1e3
전재우 <crossly_t@naver.com>
2024-02-14 23:57:50 +0900

    cache_error_edit

--- easyFront/public/serviceWorker.js
+++ easyFront/public/serviceWorker.js
@@ -1,8 +1,26 @@
 /* eslint-disable no-restricted-globals */
+const CACHE_NAME = 'my-cache-v1';
+const urlsToCache = ["/", "/index.html", "/icon-192.png", "/icon-512.png"];
+
 self.addEventListener("install", function (event) {
   event.waitUntil(
-    caches.open("my-cache").then(function (cache) {
-      return cache.addAll(["/", "/index.html", "/icon-192.png", "/icon-512.png"]);
+    caches.open(CACHE_NAME).then(function (cache) {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+self.addEventListener("activate", function (event) {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then(function(cacheNames) {
+      return Promise.all(
+        cacheNames.map(function(cacheName) {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
     })
   );
 });
