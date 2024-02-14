ae0026ff2dfa38a790b6dbbb3bd2d5b23d386bf4
신성현 <froggy0m0a@gmail.com>
2024-02-14 16:35:14 +0900

    Fix: 웹 빌드 픽스 package.json

--- easyFront/src/App.js
+++ easyFront/src/App.js
@@ -31,7 +31,7 @@ import PlaceDetailPage from "./components/pages/Place/PlaceDetailPage";
 import PlaceEditPage from "./components/pages/Place/PlaceEditPage";
 import PlaceNeighborPage from "./components/pages/Place/PlaceNeighborPage";
 
-axios.defaults.baseURL = 'https://i10b307.p.ssafy.io:8080';
+// axios.defaults.baseURL = 'https://i10b307.p.ssafy.io:8080';
 
 const HeaderDiv = styled.div`
   z-index: 10;
