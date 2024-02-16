d3957589cc486072a2a464b00cf7d279b4ed119f
이준혁 <ljh355@naver.com>
2024-02-16 13:41:21 +0900

    Update easyFront/src/components/pages/Login/LoginPage.js
    Former-commit-id: 8f824dcbd58bd3b1de4c72def0495404aa6d707d

--- easyFront/src/components/pages/Login/LoginPage.js
+++ easyFront/src/components/pages/Login/LoginPage.js
@@ -14,10 +14,8 @@ const REDIRECT_URI =
 const KAKAO_AUTH_URL = `https://kauth.kakao.com/oauth/authorize?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=code`;
 
 const LoginPage = () => {
-  const navigate = useNavigate();
-
   useEffect(() => {
-    navigate(KAKAO_AUTH_URL);
+    window.location.href = KAKAO_AUTH_URL;
   });
 
   // console.log(CLIENT_ID);
